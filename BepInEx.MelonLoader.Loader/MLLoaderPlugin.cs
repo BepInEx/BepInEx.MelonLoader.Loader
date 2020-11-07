0393813e9a82496f386da1666d23843833db5961
Bepis <36346617+bbepis@users.noreply.github.com>
2020-11-08 07:38:08 +1100

    Use custom assembly resolution instead of relying on assembly name

--- BepInEx.MelonLoader.Loader/MLLoaderPlugin.cs
+++ BepInEx.MelonLoader.Loader/MLLoaderPlugin.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx.IL2CPP;
+﻿using System;
+using BepInEx.IL2CPP;
 using MelonLoader;
 
 namespace BepInEx.MelonLoader.Loader
@@ -8,6 +9,14 @@ namespace BepInEx.MelonLoader.Loader
     {
 	    public override void Load()
 	    {
+		    AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+		    {
+			    if (args.Name.Contains("MelonLoader.ModHandler"))
+				    return typeof(MLLoaderPlugin).Assembly;
+
+			    return null;
+		    };
+
 		    MelonLoaderBase.Initialize();
 		    MelonLoaderBase.Startup();
 	    }
